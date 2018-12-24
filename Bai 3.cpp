9556b176a7fa8f952e5981b11d0504b2504edd3d
huynhkhuyen <46100580+huynhkhuyen@users.noreply.github.com>
2018-12-24 22:12:56 +0700

    Create Bai 3.cpp

---
+++ Bai 3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<string>
+#include <string.h>
+#include<fstream>
+#include<stdlib.h>
+#include <stack>
+#include<vector>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+class CauHoi{
+	string 
+	string A;
+	string B;
+	string C;
+	string D;
+	char DA[10];
+	vector <CauHoi> CH;
+	public:
+		int Random(int n)
+	{
+  		 return rand()%(n+1);
+		}
+		void Nhap(){
+			int a = Random(3);
+			switch(a){
+			case 1:
+				cout << " De cua ban la de 1 ";
+				ifstream D1;
+				D1.open("D1.txt");
+				
+				
+				
+				
+		}
+	}
+};
