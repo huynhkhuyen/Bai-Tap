0e62506cf773bf943a6424a8e21d1e751e9ab208
huynhkhuyen <46100580+huynhkhuyen@users.noreply.github.com>
2018-12-25 09:21:45 +0700

    Update Bai 2.cpp

--- Bai 2.cpp
+++ Bai 2.cpp
@@ -1,6 +1,8 @@
-#include    "iostream"
+#include  <iostream>
+#include<string>
+#include<stdlib.h>
 using namespace std;
-typedef struct DATE
+struct DATE
 {
     int d;
     int m;
@@ -12,7 +14,7 @@ bool KiemTraDate(DATE D)
     { 
         if(D.m==2)
         {   
-            if((D.y%400==0) || (D.y%4==0 && D.y%100==0))
+            if((D.y%400==0) || (D.y%4==0 && D.y%100!=0))
             {
                 if(D.d<=29)
                     return true;
@@ -27,14 +29,14 @@ bool KiemTraDate(DATE D)
                     return false;
             }  
         }
-        if(D.m==1 || D.m==3 || D.m==5 || D.m==7 || D.m==9 || D.m==11)
+        if(D.m==1 || D.m==3 || D.m==5 || D.m==7 || D.m==8 || D.m==10 || D.m == 12)
         {
             if(D.d>31)
                 return false;
             else
                 return true;
         }  
-        if(D.m==4 || D.m==6 || D.m==8 || D.m==10 || D.m==12)
+        if(D.m==4 || D.m==6 || D.m==9 || D.m==11)
         {
             if(D.d>30)
                 return false;
@@ -51,14 +53,26 @@ bool KiemTraDate(DATE D)
 void NhapDate(DATE &D)
 {
     do
-    {  
+    {   
+    	fflush(stdin);
+		string A;
         cout<<"\nNhap vao ngay thang nam:\n";
-        cout<<"Nhap ngay: ";
-        cin>>D.d;
-        cout<<"Nhap thang: ";
-        cin>>D.m;
-        cout<<"Nhap nam: ";
-        cin>>D.y;
+        getline(cin,A);
+		char C[6];
+		C[0] = A[0];
+		C[1] = A[1];
+        D.d =atoi(C);
+        C[0] = A[3];
+		C[1] = A[4];
+        D.m = atoi(C);
+        C[0] = A[6];
+		C[1] = A[7];
+		C[2] = A[8];
+		C[3] = A[9];
+        D.y = atoi(C);
+        C[2] = '\0';
+        cout << D.d << ":" << D.m << ":" << D.y << endl;
+                      fflush(stdin);
         if(KiemTraDate(D)==false)
             cout<<"\nBan da nhap sai, moi ban nhap lai: ";
         else
@@ -91,14 +105,33 @@ DATE KhoangCachDate(DATE D1,DATE D2)
         t.y=y1*(-1);
     return t;
 }
+int TinhNgay(DATE D1, DATE D2){
+	int n = 1;
+	while(D1.d != D2.d || D1.m != D2.m || D1.y != D2.y){
+		n++;
+		if((D1.d <31 &&(D1.m==1 || D1.m==3 || D1.m==5 || D1.m==7 || D1.m==8 || D1.m==10 || D1.m==12)) || (D1.d < 30 && (D1.m==4 || D1.m==6||D1.m == 9||D1.m ==11))||((D1.d < 29)&&((D1.y%400==0) || (D1.y%4==0 && D1.y%100==0))) ||((D1.d < 28&&(D1.y%4 !=0 && D1.y%100 == 0))) )
+			D1.d++;
+		else{
+			if(D1.m < 12){
+				D1.m++;
+				D1.d = 1;	
+			}
+			else{
+				D1.y++;
+				D1.m = 1;
+				D1.d = 1;
+			}
+	}
+}	return n;
+}
 DATE	CongThem(DATE D1){
 	int n;
 	cin >> n;
 	while(n != 0){
-		if((D1.d <32 &&(D1.m==1 || D1.m==3 || D1.m==5 || D1.m==7 || D1.m==9 || D1.m==11)) || (D1.d < 31 && (D1.m==4 || D1.m==6||D1.m == 9||D1.m ==11))||((D1.d < 30)&&((D1.y%400==0) || (D1.y%4==0 && D1.y%100==0))) ||((D1.d < 29&&(D1.y%4 !=0 && D1.y%100 == 0))) )
+		if((D1.d <31 &&(D1.m==1 || D1.m==3 || D1.m==5 || D1.m==7 || D1.m==8 || D1.m==10 || D1.m==12)) || (D1.d < 30 && (D1.m==4 || D1.m==6||D1.m == 9||D1.m ==11))||((D1.d < 29)&&((D1.y%400==0) || (D1.y%4==0 && D1.y%100==0))) ||((D1.d < 28&&(D1.y%4 !=0 && D1.y%100 == 0))) )
 			D1.d++;
 		else{
-			if(D1.m < 13){
+			if(D1.m < 12){
 				D1.m++;
 				D1.d = 1;	
 			}
@@ -108,7 +141,7 @@ DATE	CongThem(DATE D1){
 				D1.d = 1;
 			}
 }	n--;
-	}
+}
 	return D1;
 	
 }
@@ -149,15 +182,17 @@ DATE TruRa(DATE D1){
 		return D1;
 }
 int main()
-{  
-             DATE D1,D2,D;
+{ 
+    DATE D1,D2,D;
     NhapDate(D1);
+// NhapDate(D2);
+   // cout << "So ngay D2 trong nam " << TinhNgay(D1,D2);
    // XuatDate(D1);
    // NhapDate(D2);
     //XuatDate(D2);
      //        cout<<"\nKhoang cach giua 2 ngay: ";
     //D=KhoangCachDate(D1,D2);
     //XuatDate(D);
-    D=TruRa(D1);
-    XuatDate(D);
-}
+   // D=TruRa(D1);
+    //XuatDate(D);
+  }
