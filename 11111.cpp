7d1555f91a3b80b68c0d54b882122cc470f052fc
huynhkhuyen <46100580+huynhkhuyen@users.noreply.github.com>
2018-12-27 11:06:27 +0700

    Create 11111.cpp

---
+++ 11111.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+int nhap();
+int main()
+{
+int x;
+printf ("\n Moi ban nhap mot so tu 0->9:");
+x=nhap();
+printf("\n So ban vua nhap la: %d",x);
+getch();
+}
+int nhap()
+{
+union REGS regs;
+int x;
+regs.h.ah=1;
+int86(0x21,&regs,&regs);
+x=regs.h.al;
+x -=0x30;
+return x;
+}
